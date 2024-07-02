018cd5726c5ef5dce3e1876072258e0ab7f2b7d9
Andres Guerrero <aguerrero@fi.uba.ar>
2024-07-02 19:46:50 -0300

    add test GifItem

--- test/components/GifItem.test.jsx
+++ test/components/GifItem.test.jsx
@@ -1,13 +1,33 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { GifItem } from '../../src/components/GifItem';
 
 describe('Pruebas en el <GiftItem />', () => {
 
+    const image = {
+        title : 'Avatar',
+        url : 'https://avatar/korra.jpg'
+    }
+
     test('Debe hacer match con el snapshot', () => {
 
-        const { container } = render(<GifItem title="Un titulo" url="https://localhost/algo.jpg" />);
+        const { container } = render(<GifItem {...image} />);
         expect( container ).toMatchSnapshot();
         
     });
+
+    test('Debe de mostrar la imagen con el URL y el ALT indicado', () => { 
+        render( <GifItem {...image} /> );
+        const { src , alt } = screen.getByRole('img');
+        expect( src ).toBe( image.url );
+        expect( alt ).toBe( image.title );
+        // expect( screen.getByRole('img').src ).toBe( image.url );
+        // expect( screen.getByRole('img').alt ).toBe( image.title );
+
+    })
+
+    test('Debe de mostrar el titulo en el componente', () => { 
+        render( <GifItem {...image} /> );
+        expect( screen.getByText( image.title ) ).toBeTruthy();
+    })
 
 });
